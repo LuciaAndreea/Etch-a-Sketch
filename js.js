0d3539c8df6eaaa9013a77568255ace2d5b9eaf7
Lucia <luciaandreeaursu@yahoo.com>
2024-10-12 16:07:21 +0300

    add random colors when squares are hoovered over

--- js.js
+++ js.js
@@ -1,8 +1,16 @@
 const gridContainer = document.querySelector(".container")
 const btn = document.getElementById("reset-button");
 
-//creearea de 256 patrate si adaugarea lor la clasa container
 
+//function for a random color when a square is hovered over
+function randomColorWhenHovered(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+//function to create a grid 
 function createGrid(squaresPerSide){
      gridContainer.innerHTML = "";
 
@@ -16,13 +24,16 @@ for(let i =0;i <totalSquares;i++){
     gridItem.style.height = `${squareSize}px`;
     
     gridItem.addEventListener("mouseover", () =>{
-        gridItem.classList.add("hovered");
+        const randomColor = randomColorWhenHovered();
+        gridItem.style.backgroundColor = randomColor;
     });
 
     gridContainer.append(gridItem);
 }
 }
 
+
+//function to reset the grid based on the user's choice
 function resetGrid(){
     let userInput = prompt("Enter the number of squares per side for the new grid (Max: 100):", 16);
     let squaresPerSide = parseInt(userInput);
